96fa670977365b2ad7c0268fa92da0ce2bd2e15c
Adam Mathieson <mathieson1222@gmail.com>
2019-03-19 14:05:40 -0700

    added endpoints and logic for specificatons

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const postsRouter = require('./posts/posts-router');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send();
+});
+
+server.use('/api/posts', postsRouter);
+
+module.exports = server;
